e4061ddf1bc4a227a44f7dd9a866954f8c5259ad
ZaripovEgor <144524848+ZaripovEgor@users.noreply.github.com>
2023-12-03 03:15:03 +0500

    Task2

---
+++ Tyuiu.ZaripovEO.Sprint6.Task2.V28.Test/DataServiceTest.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.ZaripovEO.Sprint6.Task2.V28.Lib;
+
+namespace Tyuiu.ZaripovEO.Sprint6.Task2.V28.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalc()
+        {
+            DataService ds = new DataService();
+
+            int startVal = -5;
+            int stopVal = 5;
+            int len = stopVal - startVal + 1;
+
+            double[] WaitArray;
+            WaitArray = new double[len];
+
+            WaitArray[0] = 17.27;
+            WaitArray[1] = 14.08;
+            WaitArray[2] = 10.27;
+            WaitArray[3] = 6.65;
+            WaitArray[4] = 3.87;
+            WaitArray[5] = 2;
+            WaitArray[6] = -0.74;
+            WaitArray[7] = -3.71;
+            WaitArray[8] = -7.59;
+            WaitArray[9] = -11.55;
+            WaitArray[10] = -14.74;
+
+            double[] res;
+            res = new double[len];
+            res = ds.GetMassFunction(startVal, stopVal);
+            CollectionAssert.AreEqual(WaitArray, res);
+
+        }
+    }
+}
