7604bfc92f1c46213c6828e033844648df936884
Tyuiu.ZaripovEO@outlook.com <zaripov-egor@mail.ru>
2023-12-03 13:23:03 +0500

    Task2

--- Tyuiu.ZaripovEO.Sprint6.Task2.V28.Lib/DataService.cs
+++ Tyuiu.ZaripovEO.Sprint6.Task2.V28.Lib/DataService.cs
@@ -12,7 +12,7 @@ namespace Tyuiu.ZaripovEO.Sprint6.Task2.V28.Lib
         public double[] GetMassFunction(int startValue, int stopValue)
         {
             double[] valueArray;
-            int len = (stopValue - startValue) + 1;
+            int len = (stopValue - startValue) + 1; 
             valueArray = new double[len];
             double y;
             int count = 0;
